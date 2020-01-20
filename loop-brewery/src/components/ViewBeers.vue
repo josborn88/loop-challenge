1d32317caaa90e1f4069e7f6fab83c6c7b3dab00
John <Josborn88@gmail.com>
2020-01-20 10:20:52 -0500

    created basic vue project, ViewBeer component, and connect to eh api successfully

---
+++ loop-brewery/src/components/ViewBeers.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Beer goes here</h1>
+    <div class="beer-list" v-for="beer in beers" :key="beer.id">
+      <p>{{ beer.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "view-beers",
+  data() {
+    return {
+      beers: [],
+      apiURL: "https://api.punkapi.com/v2/beers?page=1&per_page=25"
+    };
+  },
+  methods: {
+    getBeers() {
+      fetch(this.apiURL, {
+        headers: {}
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          this.beers = data;
+        })
+        .catch(err => console.error(err));
+    }
+  },
+  created() {
+    this.getBeers();
+  }
+};
+</script>
+
+<style></style>
---
+++ loop-brewery/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ViewBeers />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import ViewBeers from "@/components/ViewBeers.vue";
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    ViewBeers
+  }
+};
+</script>
