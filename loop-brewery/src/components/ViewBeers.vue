74ee8f998c8a59b4abec1bb92349955134bc1025
John <Josborn88@gmail.com>
2020-01-20 10:56:22 -0500

    able to get a page of beers fro mthe API, and can change which page we display

--- loop-brewery/src/components/ViewBeers.vue
+++ loop-brewery/src/components/ViewBeers.vue
@@ -2,8 +2,18 @@
   <div>
     <h1>Beer goes here</h1>
     <div class="beer-list" v-for="beer in beers" :key="beer.id">
-      <p>{{ beer.name }}</p>
+      <div class="beer">
+        <div class="beer-description">
+          <p>{{ beer.name }}</p>
+          <p>{{ beer.description }}</p>
+        </div>
+        <div class="beer-img">
+          <img :src="beer.image_url" />
+        </div>
+      </div>
     </div>
+    <p v-on:click.prevent="getBeers(2)">Page 2</p>
+    <p v-on:click.prevent="getBeers(3)">Page 3</p>
   </div>
 </template>
 
@@ -12,13 +22,14 @@ export default {
   name: "view-beers",
   data() {
     return {
+      num: 1,
       beers: [],
       apiURL: "https://api.punkapi.com/v2/beers?page=1&per_page=25"
     };
   },
   methods: {
-    getBeers() {
-      fetch(this.apiURL, {
+    getBeers(num) {
+      fetch(`https://api.punkapi.com/v2/beers?page=${num}&per_page=25`, {
         headers: {}
       })
         .then(response => {
@@ -31,7 +42,7 @@ export default {
     }
   },
   created() {
-    this.getBeers();
+    this.getBeers(this.num);
   }
 };
 </script>
--- loop-brewery/src/views/Home.vue
+++ loop-brewery/src/views/Home.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <ViewBeers />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import ViewBeers from "@/components/ViewBeers.vue";
 export default {
   name: "home",
   components: {
-    HelloWorld,
     ViewBeers
   }
 };
